6d9ca2dfb81d25e959bfc7179868803439c33f4f
Gabriel <gabigolpaiva@gmail.com>
2020-11-09 10:58:14 -0300

    Organizing python scripts in a folder

---
+++ python_scripts/loop_layer.py
@@ -0,0 +1,8 @@
+from output_layer import display_menu, clear_screen
+from input_layer import get_option
+
+def program_looper():
+    #While(True):
+    clear_screen()
+    display_menu(0)
+    option = get_option()
